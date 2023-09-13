67db0e6c6d3c5a87dfd1d860bc6e40ce6391bf2c
fengzai6 <lfq0120@gmail.com>
2023-09-13 22:35:01 +0800

    123

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://我不是.肥猫.love:63/接口禁止贩卖");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://www.qqwj.live/tvbox/tv1.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
